136116ef5d5a0fc42babc8af6030caf96d812faf
ahakola <40722602+ahakola@users.noreply.github.com>
2024-09-03 02:27:29 +0300

    Add TWW SkillLines to the addon

--- DMFQuest.lua
+++ DMFQuest.lua
@@ -159,7 +159,7 @@
 	-- Portal Areas
 		-- For some reason areaIDs change between Retail and CataClassic while subZoneAreaIDs stay the same?
 		local capitalCityAreaIDs = {
-			-- https://wow.tools/dbc/?dbc=uimap
+			-- https://wago.tools/db2/UiMap // https://wow.tools/dbc/?dbc=uimap
 			-- Alliance
 			[isRetail and 84 or 1453] = true, -- Stormwind City
 			[isRetail and 87 or 1455] = true, -- Ironforge
@@ -173,8 +173,8 @@
 		}
 		local subZoneAreaIDs = { -- uiMapIDs and their matching subZone areaIDs
 			--[[
-			-- https://wow.tools/dbc/?dbc=uimap
-			-- https://wow.tools/dbc/?dbc=areatable
+			-- https://wago.tools/db2/UiMap // https://wow.tools/dbc/?dbc=uimap
+			-- https://wago.tools/db2/AreaTable // https://wow.tools/dbc/?dbc=areatable
 			[uiMapID] = {
 				areaID,
 				areaID,
@@ -278,53 +278,53 @@
 						}
 		}
 		local ProfessionTradeSkillLines = {
-			-- https://wowpedia.fandom.com/wiki/TradeSkillLineID
-			-- https://wow.tools/dbc/?dbc=skillline
-			-- [ProfId] = { Classic, TBC, Wrath, Cata, MoP, WoD, Legion, BfA, SL, DF }
+			-- https://warcraft.wiki.gg/wiki/TradeSkillLineID // https://wowpedia.fandom.com/wiki/TradeSkillLineID
+			-- https://wago.tools/db2/SkillLine // https://wow.tools/dbc/?dbc=skillline
+			-- [ProfId] = { Classic, TBC, Wrath, Cata, MoP, WoD, Legion, BfA, SL, DF, TWW }
 			-- Primary Professions
 				[171] = { -- Alchemy
-					2485, 2484, 2483, 2482, 2481, 2480, 2479, 2478, 2750, 2823 
+					2485, 2484, 2483, 2482, 2481, 2480, 2479, 2478, 2750, 2823, 2871
 				},
 				[164] = { -- Blacksmithing
-					2477, 2476, 2475, 2474, 2473, 2472, 2454, 2437, 2751, 2822
+					2477, 2476, 2475, 2474, 2473, 2472, 2454, 2437, 2751, 2822, 2872
 				},
 				[333] = { -- Enchanting
-					2494, 2493, 2492, 2491, 2489, 2488, 2487, 2486, 2753, 2825
+					2494, 2493, 2492, 2491, 2489, 2488, 2487, 2486, 2753, 2825, 2874
 				},
 				[202] = { -- Engineering
-					2506, 2505, 2504, 2503, 2502, 2501, 2500, 2499, 2755, 2827
+					2506, 2505, 2504, 2503, 2502, 2501, 2500, 2499, 2755, 2827, 2875
 				},
 				[182] = { -- Herbalism
-					2556, 2555, 2554, 2553, 2552, 2551, 2550, 2549, 2760, 2832
+					2556, 2555, 2554, 2553, 2552, 2551, 2550, 2549, 2760, 2832, 2877
 				},
 				[773] = { -- Inscription
-					2514, 2513, 2512, 2511, 2510, 2509, 2508, 2507, 2756, 2828
+					2514, 2513, 2512, 2511, 2510, 2509, 2508, 2507, 2756, 2828, 2878
 				},
 				[755] = { -- Jewelcrafting
-					2524, 2523, 2522, 2521, 2520, 2519, 2518, 2517, 2757, 2829
+					2524, 2523, 2522, 2521, 2520, 2519, 2518, 2517, 2757, 2829, 2879
 				},
 				[165] = { -- Leatherworking
-					2532, 2531, 2530, 2529, 2528, 2527, 2526, 2525, 2758, 2830
+					2532, 2531, 2530, 2529, 2528, 2527, 2526, 2525, 2758, 2830, 2880
 				},
 				[186] = { -- Mining
-					2572, 2571, 2570, 2569, 2568, 2567, 2566, 2565, 2761, 2833
+					2572, 2571, 2570, 2569, 2568, 2567, 2566, 2565, 2761, 2833, 2881
 				},
 				[393] = { -- Skinning
-					2564, 2563, 2562, 2561, 2560, 2559, 2558, 2557, 2762, 2834
+					2564, 2563, 2562, 2561, 2560, 2559, 2558, 2557, 2762, 2834, 2882
 				},
 				[197] = { -- Tailoring
-					2540, 2539, 2538, 2537, 2536, 2535, 2534, 2533, 2759, 2831
+					2540, 2539, 2538, 2537, 2536, 2535, 2534, 2533, 2759, 2831, 2883
 				},
 			-- Secondary Professions
 				--[794] = { -- Archeology
 				--}
 				[185] = { -- Cooking
-					2548, 2547, 2546, 2545, 2544, 2543, 2542, 2541, 2752, 2824
+					2548, 2547, 2546, 2545, 2544, 2543, 2542, 2541, 2752, 2824, 2873
 				},
 				--[129] = { -- First Aid
 				--},
 				[356] = { -- Fishing
-					2592, 2591, 2590, 2589, 2588, 2587, 2586, 2585, 2754, 2826
+					2592, 2591, 2590, 2589, 2588, 2587, 2586, 2585, 2754, 2826, 2876
 				}
 		}
 
